ffd47e648a787dc4f5be8305d2340aaa77fca67f
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-15 14:41:20 +0530

    Score Added

--- script.js
+++ script.js
@@ -53,13 +53,16 @@ const gameEngine = () => {
         snakeArray = [{ x: 13, y: 15 }]
         // musicSound.play()
         score = 0;
+        speed = 2;
     }
 
     // If snake have eaten the food, increment the score and regenerate the food
     if (snakeArray[0].x === food.x && snakeArray[0].y === food.y) {
         foodSound.play()
-        speed++;
         score++;
+        console.log(score);
+        scoreBox.innerHTML = "Score : " + score
+        speed++;
         snakeArray.unshift({ x: snakeArray[0].x + direction.x, y: snakeArray[0].y + direction.y })
         food = {
             x: Math.floor(Math.random() * (17 - 2 + 1)) + 2,
@@ -105,22 +108,22 @@ window.addEventListener('keydown', (e) => {
     moveSound.play()
     switch (e.key) {
         case "ArrowUp":
-            console.log("ArrowUp Key Pressed");
+            // console.log("ArrowUp Key Pressed");
             direction.x = 0;
             direction.y = -1;
             break;
         case "ArrowDown":
-            console.log("ArrowDown Key Pressed");
+            // console.log("ArrowDown Key Pressed");
             direction.x = 0;
             direction.y = 1;
             break;
         case "ArrowLeft":
-            console.log("ArrowLeft Key Pressed");
+            // console.log("ArrowLeft Key Pressed");
             direction.x = -1;
             direction.y = 0;
             break;
         case "ArrowRight":
-            console.log("ArrowRight Key Pressed");
+            // console.log("ArrowRight Key Pressed");
             direction.x = 1;
             direction.y = 0;
             break;
