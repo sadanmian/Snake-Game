7e22bb7590d737f3ff02c19db208bcf01c79b6a3
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-15 15:44:41 +0530

    Bugs Remove

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
 // Game Constants
-
 let direction = { x: 0, y: 0 }
 const foodSound = new Audio('music/food.mp3')
 const gameOverSound = new Audio('music/gameover.mp3')
 const moveSound = new Audio('music/move.mp3')
-const musicSound = new Audio('music/music.mp3')
 let score = 0;
 let speed = 2;
 let lastPaint_time = 0;
-let snakeArray = [{ x: 13, y: 15 }]
-
-// let food = { x: 6, y: 16 }
-// min = 2 and max = 17
-// var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+let snakeArray = [{ x: 9, y: 9 }]
 let food = {
     x: Math.floor(Math.random() * (17 - 2 + 1)) + 2,
     y: Math.floor(Math.random() * (17 - 2 + 1)) + 2
 }
-
 // Game Functions
-
 const main = (current_time) => {
     window.requestAnimationFrame(main)
-    // console.log(current_time);
     if ((current_time - lastPaint_time) / 1000 < 1 / speed) {
         return;
     }
     lastPaint_time = current_time
     gameEngine()
 }
-
 function isCollide(snake) {
     // if you collide with yourself
     for (let i = 1; i < snakeArray.length; i++) {
@@ -42,46 +32,44 @@ function isCollide(snake) {
         return true
     }
 }
-
 const gameEngine = () => {
-    //Part 1: Updating the Snake Array & food
+    //Part 1: Updating the Snake Length & food
     if (isCollide(snakeArray)) {
         gameOverSound.play()
-        musicSound.pause()
         direction = { x: 0, y: 0 }
         alert("Game Over. Press any key to play again")
         snakeArray = [{ x: 13, y: 15 }]
-        score = 0;
+        scoreBox.innerHTML = `Socre : 0`
         speed = 2;
     }
-
     // If snake have eaten the food, increment the score and regenerate the food
     if (snakeArray[0].x === food.x && snakeArray[0].y === food.y) {
         foodSound.play()
         score++;
         speed++;
-        // console.log(score);
-        // scoreBox.innerHTML = "Score : " + score
         scoreBox.innerHTML = `Score : ${score}`
         if (score > hiScoreVal) {
             hiScoreVal = score
             localStorage.setItem('hiscore', JSON.stringify(hiScoreVal))
             highScoreBox.innerHTML = "HiScore : " + hiScoreVal
         }
-        snakeArray.unshift({ x: snakeArray[0].x + direction.x, y: snakeArray[0].y + direction.y })
+        snakeArray.unshift(
+            {
+                x: snakeArray[0].x + direction.x,
+                y: snakeArray[0].y + direction.y
+            }
+        )
         food = {
             x: Math.floor(Math.random() * (17 - 2 + 1)) + 2,
             y: Math.floor(Math.random() * (17 - 2 + 1)) + 2
         }
     }
-
     // Moving the Snake
     for (let i = snakeArray.length - 2; i >= 0; i--) {
         snakeArray[i + 1] = { ...snakeArray[i] }
     }
     snakeArray[0].x = snakeArray[0].x + direction.x
     snakeArray[0].y = snakeArray[0].y + direction.y
-
     // Part 2: Display the Snake & Food
     // Display the Snake
     board.innerHTML = ""
@@ -104,9 +92,7 @@ const gameEngine = () => {
     foodElement.classList.add('food')
     board.appendChild(foodElement)
 }
-
 // Main Logic
-
 // Login for High Score by using local Storage
 let hiscore = localStorage.getItem("hiscore")
 if (hiscore === null) {
@@ -117,29 +103,24 @@ else {
     hiScoreVal = JSON.parse(hiscore)
     highScoreBox.innerHTML = `HiScore : ${hiscore}`
 }
-
 window.requestAnimationFrame(main)
 window.addEventListener('keydown', (e) => {
     direction = { x: 0, y: 1 }  // Start the Game
     moveSound.play()
     switch (e.key) {
         case "ArrowUp":
-            // console.log("ArrowUp Key Pressed");
             direction.x = 0;
             direction.y = -1;
             break;
         case "ArrowDown":
-            // console.log("ArrowDown Key Pressed");
             direction.x = 0;
             direction.y = 1;
             break;
         case "ArrowLeft":
-            // console.log("ArrowLeft Key Pressed");
             direction.x = -1;
             direction.y = 0;
             break;
         case "ArrowRight":
-            // console.log("ArrowRight Key Pressed");
             direction.x = 1;
             direction.y = 0;
             break;
